3b73a087187951f3c19c5efb70a147253a440e35
Function Forrest <Function.forrest@gmail.com>
2024-10-24 09:51:16 -0400

    Refactor main.py to handle exceptions when fetching country information

--- main.py
+++ main.py
@@ -5,17 +5,20 @@ while True:
     url = f"https://restcountries.com/v3.1/name/{country}"
 
     if country:
-        response = requests.get(url).json()
-        if country.lower() == "quit" or country.lower() == "q":
-            break
-        country_info = response[0]
-        capital = country_info["capital"][0]
-        region = country_info["region"]
-        population = country_info["population"]
-        population = format(population, ",d")
-        languages = country_info["languages"]
-        languages = [languages[language] for language in languages]
-        print(f"Capital: {capital}")
-        print(f"Region: {region}")
-        print(f"Population: {population}")
-        print(f"Languages: {",".join(languages)}")
+        try:
+            response = requests.get(url).json()
+            if country.lower() == "quit" or country.lower() == "q":
+                break
+            country_info = response[0]
+            capital = country_info["capital"][0]
+            region = country_info["region"]
+            population = country_info["population"]
+            population = format(population, ",d")
+            languages = country_info["languages"]
+            languages = [languages[language] for language in languages]
+            print(f"Capital: {capital}")
+            print(f"Region: {region}")
+            print(f"Population: {population}")
+            print(f"Languages: {",".join(languages)}")
+        except Exception as e:
+            print(f"An error occurred when fetching info: {e}")
